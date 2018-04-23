9d5c85f69951e0ed2f5c672213ade8b032c6fd15
Samantha Ferreira <samanthamferr@gmail.com>
2018-04-23 17:50:35 +0200

    log out button

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-'use strict'
-const express = require('express')
-const path = require('path')
-const cookieParser = require('cookie-parser')
-const logger = require('morgan')
-const mongoose = require('mongoose')
-const session = require('express-session')
-const MongoStore = require('connect-mongo')(session)
-const flash = require('connect-flash')
+'use strict';
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const flash = require('connect-flash');
 
-const indexRouter = require('./routes/index')
-const authRouter = require('./routes/auth')
+const indexRouter = require('./routes/index');
+const authRouter = require('./routes/auth');
 
-const app = express()
+const app = express();
 
 // connects to database
-mongoose.Promise = Promise
+mongoose.Promise = Promise;
 mongoose.connect('mongodb://localhost/database-name', {
   keepAlive: true,
   reconnectTries: Number.MAX_VALUE
-})
+});
 
 // session setup
 app.use(session({
@@ -32,45 +32,45 @@ app.use(session({
   cookie: {
     maxAge: 24 * 60 * 60 * 1000
   }
-}))
+}));
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 
 app.use(function (req, res, next) {
-  app.locals.xxxx = req.session.currentUser
-  next()
-})
+  app.locals.user = req.session.currentUser;
+  next();
+});
 
-app.use(logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(flash())
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(flash());
 
-app.use('/', indexRouter)
-app.use('/auth', authRouter)
+app.use('/', indexRouter);
+app.use('/auth', authRouter);
 
 // -- 404 and error handler
 
 // NOTE: requires a views/not-found.ejs template
 app.use((req, res, next) => {
-  res.status(404)
-  res.render('not-found')
-})
+  res.status(404);
+  res.render('not-found');
+});
 
 // NOTE: requires a views/error.ejs template
 app.use((err, req, res, next) => {
   // always log the error
-  console.error('ERROR', req.method, req.path, err)
+  console.error('ERROR', req.method, req.path, err);
 
   // only render if the error ocurred before sending the response
   if (!res.headersSent) {
-    res.status(500)
-    res.render('error')
+    res.status(500);
+    res.render('error');
   }
-})
+});
 
-module.exports = app
+module.exports = app;
--- routes/auth.js
+++ routes/auth.js
@@ -1,102 +1,100 @@
-'use strict'
+'use strict';
 
-const express = require('express')
-const router = express.Router()
-const bcrypt = require('bcrypt')
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
 
-const User = require('../models/user')
+const User = require('../models/user');
 
-const bcryptSalt = 10
+const bcryptSalt = 10;
 
 // get users listing
 router.get('/signup', (req, res, next) => {
   if (req.session.currentUser) {
-    res.redirect('/dog-list')
-    return
+    res.redirect('/dog-list');
+    return;
   }
 
   const data = {
-    messages: req.flash('signup') 
-  }
-  res.render('auth/signup', data)
-})
+    messages: req.flash('signup')
+  };
+  res.render('auth/signup', data);
+});
 
 router.post('/signup', (req, res, next) => {
   if (req.session.currentUser) {
-    res.redirect('/dog-list')
-    return
+    res.redirect('/dog-list');
+    return;
   }
 
   if (!req.body.username || !req.body.password) {
-    req.flash('signup', 'username or password missing')
-    res.redirect('/auth/signup')
+    req.flash('signup', 'username or password missing');
+    res.redirect('/auth/signup');
   }
 
   User.findOne({username: req.body.username})
     .then((result) => {
       if (result) {
-        console.log("username taken")
-        req.flash('signup', 'username already taken')
-        res.redirect('/auth/signup')
-        return
+        console.log('username taken');
+        req.flash('signup', 'username already taken');
+        res.redirect('/auth/signup');
+        return;
       }
-     
-      const salt = bcrypt.genSaltSync(bcryptSalt)
-      const hashPass = bcrypt.hashSync(req.body.password, salt)
+
+      const salt = bcrypt.genSaltSync(bcryptSalt);
+      const hashPass = bcrypt.hashSync(req.body.password, salt);
 
       const newUser = new User({
         username: req.body.username,
         password: hashPass
-      })
+      });
 
       newUser.save() // once new user is saved, store in the session and send to dog-list
         .then((user) => {
-          req.session.currentUser = newUser
-          res.redirect('/dog-list')
+          req.session.currentUser = newUser;
+          res.redirect('/dog-list');
         })
-        .catch(next)
+        .catch(next);
     })
-    .catch(next)
-})
+    .catch(next);
+});
 
-router.get('/login', (req,res,next) =>{
+router.get('/login', (req, res, next) => {
   if (req.session.currentUser) {
-    res.redirect('/dog-list')
-    return
-  }
-
-  const data = {  //receives log in flash message
-    messages: req.flash('login')
-  }
-  res.render('auth/login', data)  
-});
-
-router.post('/login', (req,res,next)=>{
-  User.findOne({username: req.body.username})
-  .then((result) => {
-    if (!result){
-      req.flash('login', 'username not found')
-      console.log("user not found")
-      res.redirect('/auth/login');
-      return;
-    }
-    if (!bcrypt.compareSync(req.body.password , result.password)){ 
-      req.flash('login', 'incorrect password');
-      console.log("incorrect password")
-      res.redirect('/auth/login');
-      return;
-    }
-    req.session.currentUser = result;
     res.redirect('/dog-list');
-  })
-  .catch(next);
-})
+    return;
+  }
 
-router.post('/logout', (req,res,next) => {
-  req.session.currentUser = null;
-  res.redirect('/login');
+  const data = { // receives log in flash message
+    messages: req.flash('login')
+  };
+  res.render('auth/login', data);
 });
 
+router.post('/login', (req, res, next) => {
+  User.findOne({username: req.body.username})
+    .then((result) => {
+      if (!result) {
+        req.flash('login', 'username not found');
+        console.log('user not found');
+        res.redirect('/auth/login');
+        return;
+      }
+      if (!bcrypt.compareSync(req.body.password, result.password)) {
+        req.flash('login', 'incorrect password');
+        console.log('incorrect password');
+        res.redirect('/auth/login');
+        return;
+      }
+      req.session.currentUser = result;
+      res.redirect('/dog-list');
+    })
+    .catch(next);
+});
 
+router.post('/logout', (req, res, next) => {
+  req.session.currentUser = null;
+  res.redirect('/auth/login');
+});
 
 module.exports = router;
