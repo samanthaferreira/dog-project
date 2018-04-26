e181a022e11c9c953613ef20f65252bb3e401595
Samantha Ferreira <samanthamferr@gmail.com>
2018-04-26 15:27:50 +0200

    added locations on map

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,6 +1,12 @@
 function startMap () {
   // Store Ironhack's coordinates
-  const dogPark1 = { lat: 41.389346, lng: 2.180464 };
+  const dogPark1 = { lat: 41.3893, lng: 2.1804 };
+  const dogPark2 = { lat: 41.3881, lng: 2.1860 };
+  const dogPark3 = { lat: 41.3932, lng: 2.1835};
+  const dogPark4 = { lat: 41.4001, lng: 2.1674};
+  const dogPark5 = { lat: 41.4145, lng: 2.1527};
+  const dogPark6 = {lat: 41.382862, lng: 2.192674};
+  const dogPark7 = {lat: 41.407523, lng: 2.200871};
 
   // Map initialization
   const map = new google.maps.Map(document.getElementById('map'), {
@@ -14,8 +20,65 @@ function startMap () {
       lat: dogPark1.lat,
       lng: dogPark1.lng
     },
+
     map: map,
-    title: 'Barcelona Campus'
+    title: 'Dog Park'
+  });
+
+  const dogPark2Marker = new google.maps.Marker({
+    position: {
+      lat: dogPark2.lat,
+      lng: dogPark2.lng
+    },
+
+    map: map,
+    title: 'Dog Park'
+  });
+  const dogPark3Marker = new google.maps.Marker({
+    position: {
+      lat: dogPark3.lat,
+      lng: dogPark3.lng
+    },
+
+    map: map,
+    title: 'Dog Park'
+  });
+  const dogPark4Marker = new google.maps.Marker({
+    position: {
+      lat: dogPark4.lat,
+      lng: dogPark4.lng
+    },
+
+    map: map,
+    title: 'Dog Park'
+  });
+
+  const dogPark5Marker = new google.maps.Marker({
+    position: {
+      lat: dogPark5.lat,
+      lng: dogPark5.lng
+    },
+
+    map: map,
+    title: 'Dog Park'
+  });
+  const dogPark6Marker = new google.maps.Marker({
+    position: {
+      lat: dogPark6.lat,
+      lng: dogPark6.lng
+    },
+
+    map: map,
+    title: 'Dog Park'
+  });
+  const dogPark7Marker = new google.maps.Marker({
+    position: {
+      lat: dogPark7.lat,
+      lng: dogPark7.lng
+    },
+
+    map: map,
+    title: 'Dog Park'
   });
 
   if (navigator.geolocation) {
