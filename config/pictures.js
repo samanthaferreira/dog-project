2ebe42b99dc83ea04014948e38b627cb14bb0d55
Samantha Ferreira <samanthamferr@gmail.com>
2018-04-26 18:12:42 +0200

    fixed section class

--- config/pictures.js
+++ config/pictures.js
@@ -3,7 +3,21 @@
 
 const pictures = [
   '/images/profile-images/dogs/dog1.jpg',
-  '/images/profile-images/dogs/dog2.jpg'
+  '/images/profile-images/dogs/dog2.jpg',
+  '/images/profile-images/dogs/dog3.jpeg',
+  '/images/profile-images/dogs/dog4.jpg',
+  '/images/profile-images/dogs/dog5.jpg',
+  '/images/profile-images/dogs/dog6.jpeg',
+  '/images/profile-images/dogs/dog7.jpg',
+  '/images/profile-images/dogs/dog8.jpg',
+  '/images/profile-images/dogs/dog9.jpeg',
+  '/images/profile-images/dogs/dog10.jpeg',
+  '/images/profile-images/dogs/dog11.jpg',
+  '/images/profile-images/dogs/dog12.jpeg',
+  '/images/profile-images/dogs/dog13.jpg',
+  '/images/profile-images/dogs/dog14.jpg',
+  '/images/profile-images/dogs/dog15.jpg'
+
 ];
 
 module.exports = pictures;
