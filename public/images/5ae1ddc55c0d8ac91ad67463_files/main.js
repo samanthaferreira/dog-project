5cd2be5c74ab1f0d81d790962bded981e4c2c5e4
Ruben Aguayo <Pelao@MacBook-Pro-de-Ruben.local>
2018-04-26 18:08:31 +0200

    changes on dog-more and its css

---
+++ public/images/5ae1ddc55c0d8ac91ad67463_files/main.js
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title></title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel='stylesheet' href='/stylesheets/style.css' />
+    <link href='https://fonts.googleapis.com/css?family=Cookie' rel='stylesheet' type='text/css'>
+  </head>
+  <body>
+    <header id="site-header">
+      <div class="container">
+        <a href="/dog-list" class="woof">Woof!</a>
+        
+       <p class="loggedInAs">Logged in as toto</p>
+        <form method="POST" action="/auth/logout">
+          <button class="logoutBtn">Logout</button>
+        </form>
+        
+      
+    
+      </div>
+    </header>
+
+    <div class="container">
+    
+      <div class="section">
+  <h1>Not-Found, </h1>
+  <img src="/images/not-found.gif">
+</div>
+      
+    </div>
+<script  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAShHBa3zYWUqcop_dNNEdbtSU7FVm2_KE"></script>
+<script src="./javascript/main.js"></script>
+           
+    <footer id="site-footer">
+      <div class="container ">
+     <p>&copy 2018</p><p class="woof-footer">Woof</p>
+      </div>
+    </footer>
+  </body>
+</html>
