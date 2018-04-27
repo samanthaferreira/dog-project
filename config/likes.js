bc25a97df0efdad52c737d7f7a5b4774d0943530
Samantha Ferreira <samanthamferr@gmail.com>
2018-04-27 15:35:26 +0200

    added photo width and height

--- config/likes.js
+++ config/likes.js
@@ -1,7 +1,16 @@
 'use strict';
 
 const likes = {
-  'likes-dogs': 'Likes playing with other dogs'
+  'likes-dogs': 'Likes playing with other dogs',
+  'beach': 'Beachy Dog',
+  'shy': 'shy',
+  'playful': 'playful',
+  'party': 'Party animal',
+  'sleepy': 'Sleeps like a baby',
+  'head': 'Cutest head tilt',
+  'tail': 'Chases own tail',
+  'food': 'FOOD?! Where?!'
+
 };
 
 module.exports = likes;
